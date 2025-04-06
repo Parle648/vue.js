67aee997f0e9cacf71c5504d0733e8cbf300774a
Olexandr Morozov <106087433+Parle648@users.noreply.github.com>
2025-04-06 12:36:41 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <!--  <main class="bg-gray-300 h-screen h-full">-->
+  <!--    <RouterView />-->
+  <!--  </main>-->
+  <main class="bg-gray-100 min-h-screen flex flex-col">
+    <div class="bg-white shadow">
+      <div class="container mx-auto px-4 py-2 flex items-center justify-between">
+        <h1 class="text-xl font-bold">Todo App with Pinia and TailwindCSS</h1>
+      </div>
+    </div>
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/todolists/:id',
+      name: 'todolist',
+      component: () => import('@/views/TodolistView.vue'),
+    },
+  ],
+});
+
+export default router;
